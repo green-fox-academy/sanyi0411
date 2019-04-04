b7eead31f6dd54dd33e3ae6f3f24642a1f5b7147
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 11:32:06 +0200

    2. Greeter function completed

---
+++ week-01/day-4/2. Greeter function/cmake-build-debug/CMakeFiles/2__Greeter_function.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2__Greeter_function.dir/main.cpp.obj"
+  "2__Greeter_function.pdb"
+  "2__Greeter_function.exe"
+  "2__Greeter_function.exe.manifest"
+  "lib2__Greeter_function.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2__Greeter_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
