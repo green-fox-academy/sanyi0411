2637b0d3ac9e54eb1737a0eed9b19d0360bf1b2b
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 12:46:24 +0200

    10. Change element completed

---
+++ week-01/day-4/10. Change element/cmake-build-debug/CMakeFiles/10__Change_element.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10__Change_element.dir/main.cpp.obj"
+  "10__Change_element.pdb"
+  "10__Change_element.exe"
+  "10__Change_element.exe.manifest"
+  "lib10__Change_element.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10__Change_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
