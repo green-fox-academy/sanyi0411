4894df4668543e2d18e3057813396ecb923e55f4
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 11:32:42 +0200

    3. Append a completed

---
+++ week-01/day-4/3. Append a/cmake-build-debug/CMakeFiles/3__Append_a.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Sulis/Greenfox/sanyi0411/week-01/day-4/3. Append a/main.cpp" "D:/Sulis/Greenfox/sanyi0411/week-01/day-4/3. Append a/cmake-build-debug/CMakeFiles/3__Append_a.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ week-01/day-4/3. Append a/cmake-build-debug/CMakeFiles/3__Append_a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3__Append_a.dir/main.cpp.obj"
+  "3__Append_a.pdb"
+  "3__Append_a.exe"
+  "3__Append_a.exe.manifest"
+  "lib3__Append_a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3__Append_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
