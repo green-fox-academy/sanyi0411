64576b46d0d0c9dd60e3af61123d05f94643782d
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 08:53:54 +0200

    5. FavoriteNumber completed

---
+++ week-01/day-3/5. FavoriteNumber/cmake-build-debug/CMakeFiles/5__FavoriteNumber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5__FavoriteNumber.dir/main.cpp.obj"
+  "5__FavoriteNumber.pdb"
+  "5__FavoriteNumber.exe"
+  "5__FavoriteNumber.exe.manifest"
+  "lib5__FavoriteNumber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5__FavoriteNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
