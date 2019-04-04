970982bf45fc0ed760f8eb9a22ed3156e95cd883
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 11:20:31 +0200

    1. Doubling completed

---
+++ week-01/day-4/1. Doubling/cmake-build-debug/CMakeFiles/1__Doubling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1__Doubling.dir/main.cpp.obj"
+  "1__Doubling.pdb"
+  "1__Doubling.exe"
+  "1__Doubling.exe.manifest"
+  "lib1__Doubling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1__Doubling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-01/day-4/1. Doubling/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+
+int doubling (int);
+
+
+int main(int argc, char* args[]) {
+
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    // - Print the result of `doubling(baseNum)`
+
+    int baseNum = 123;
+
+    std::cout << doubling(baseNum) << std::endl;
+
+    return 0;
+}
+
+int doubling (int a) {
+    a *= 2;
+    return a;
+}
