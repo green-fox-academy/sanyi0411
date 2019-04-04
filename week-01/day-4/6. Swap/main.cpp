11e5f53f6bb6a563473e087cb159787f82344a1a
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 12:02:25 +0200

    6. Swap completed

--- week-01/day-4/6. Swap/main.cpp
+++ week-01/day-4/6. Swap/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 
+
+void swap (int&, int&);
+
 int main(int argc, char* args[]) {
     // Create two integer variables: a=15, b=23
     // Create a function named swap that gets the 2 integers as parameters and swaps its values.
     // Print the values before and after the function call and make sure the values are printed in reversed order.
 
+    int a = 15;
+    int b = 23;
+
+    std::cout << "Values before the swap: \n";
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    swap (a, b);
+
+    std::cout << "\nValues after the swap: \n";
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
 return 0;
+}
+
+void swap (int& x, int& y) {
+    int c = x;
+    x = y;
+    y = c;
 }
