0e28dfdc494fd036884427416f305f70bd554ca0
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 08:55:50 +0200

    8. DefineBasicInfo

---
+++ week-01/day-3/8. DefineBasicInfo/cmake-build-debug/CMakeFiles/8__DefineBasicInfo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8__DefineBasicInfo.dir/main.cpp.obj"
+  "8__DefineBasicInfo.pdb"
+  "8__DefineBasicInfo.exe"
+  "8__DefineBasicInfo.exe.manifest"
+  "lib8__DefineBasicInfo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8__DefineBasicInfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-01/day-3/8. DefineBasicInfo/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+int main(int argc, char* args[]) {
+
+    // Define several things as a variable then print their values
+    // Your name as a string
+    std::string myName = "Sandor Derda";
+    std::cout << myName << std::endl;
+
+    // Your age as an integer
+    int myAge = 27;
+    std::cout << myAge << std::endl;
+
+    // Your height in meters as a double
+    double myHeight = 1.91;
+    std::cout << myHeight << std::endl;
+
+    // Whether you are married or not as a boolean
+    bool married = false;
+    std::cout << married << std::endl;
+
+return 0;
+}
