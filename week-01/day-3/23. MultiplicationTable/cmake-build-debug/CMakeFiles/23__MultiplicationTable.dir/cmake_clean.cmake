9aea3dd286647a7b3e3e78578c3aec0940b93402
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 09:05:43 +0200

    23. MultiplicationTable completed

---
+++ week-01/day-3/23. MultiplicationTable/cmake-build-debug/CMakeFiles/23__MultiplicationTable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/23__MultiplicationTable.dir/main.cpp.obj"
+  "23__MultiplicationTable.pdb"
+  "23__MultiplicationTable.exe"
+  "23__MultiplicationTable.exe.manifest"
+  "lib23__MultiplicationTable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/23__MultiplicationTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
