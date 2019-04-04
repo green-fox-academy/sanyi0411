9a2e6d40447f1c7b963838d7cfd8ca4e84b32bcc
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 12:40:01 +0200

    9. Sum elements completed

--- week-01/day-4/9. Sum elements/main.cpp
+++ week-01/day-4/9. Sum elements/main.cpp
@@ -7,5 +7,10 @@ int main(int argc, char* args[]) {
     //   with the following content: `[54, 23, 66, 12]`
     // - Print the sum of the second and the third element
 
+    int r [4] = {54, 23, 66, 12};
+
+    int sum = r[1] + r[2];
+    std::cout << sum << std::endl;
+
 return 0;
 }
