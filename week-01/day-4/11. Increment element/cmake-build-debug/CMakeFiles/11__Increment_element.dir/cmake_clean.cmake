f27c8deb6d3466c46e9c243b1523aba85f19ec1b
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-04 12:51:06 +0200

    11. Increment element completed

---
+++ week-01/day-4/11. Increment element/cmake-build-debug/CMakeFiles/11__Increment_element.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11__Increment_element.dir/main.cpp.obj"
+  "11__Increment_element.pdb"
+  "11__Increment_element.exe"
+  "11__Increment_element.exe.manifest"
+  "lib11__Increment_element.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11__Increment_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
