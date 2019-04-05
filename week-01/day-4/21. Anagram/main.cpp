821e72a2d366e27ecd7e540360a14ecfaf71343a
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-05 13:47:40 +0200

    21. Anagram completed

--- week-01/day-4/21. Anagram/main.cpp
+++ week-01/day-4/21. Anagram/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <string>
 
-void swap (int& a, int& b) {
+void swap (int &a, int &b) {
     int s = a;
     a = b;
     b = s;
 }
 
 void bubble (int arr[], int size) {
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < size - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap (arr[j], arr[j + 1]);
             }
         }
     }
@@ -19,11 +19,12 @@ void bubble (int arr[], int size) {
 
 bool anagram (std::string a, std::string b) {
     int aa[a.length()];
-    int bb[a.length()];
-    bool f = true;
+    int bb[b.length()];
+    bool f = false;
     int tr = 0;
+
     if (a.length() != b.length()) {
-        f = false;
+        f = false;                                  //If they are not the same length, they cannot be anagrams
     } else {
         for (int i = 0; i < a.length(); i++) {
             aa[i] = a[i];
@@ -36,20 +37,23 @@ bool anagram (std::string a, std::string b) {
         bubble(bb, sizeof(bb) / sizeof(int));
     }
     for (int i = 0; i < a.length(); i++) {
-        std::cout << aa[i] << std::endl;
+        std::cout << aa[i] << "\t";
     }
+    std::cout<< std::endl;
 
     for (int i = 0; i < b.length(); i++) {
-        std::cout << bb[i] << std::endl;
+        std::cout << bb[i] << "\t";
     }
-    /*for (int i = 0; i < a.length(); i++) {
+    std::cout << std::endl;
+
+    for (int i = 0; i < a.length(); i++) {
         if (aa[i] == bb[i]) {
                tr += 1;
         }
         if (tr == a.length()) {
             f = true;
         }
-    }*/
+    }
     return f;
 }
 
@@ -61,8 +65,8 @@ int main(int argc, char* args[]) {
     // It should take two strings and return a boolean value
     // depending on whether its an anagram or not.
 
-    std::string a = "aaa";
-    std::string b = "&&&";
+    std::string a = "abba";
+    std::string b = "bbaa";
 
     if (anagram(a, b) == true) {
         std::cout << "They are anagrams!\n";
