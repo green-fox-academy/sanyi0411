0286309d3dcc76a948453085231e048c7aa3f5d9
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-08 21:50:21 +0200

    2. Url fixer completed

---
+++ week-02/day-2/2. Url fixer/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string url("https//www.reddit.com/r/nevertellmethebots");
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+    url.replace (url.find ("bots"), 4, "odds");
+    url.insert (5, ":");
+
+    std::cout << url << std::endl;
+
+    return 0;
+}
