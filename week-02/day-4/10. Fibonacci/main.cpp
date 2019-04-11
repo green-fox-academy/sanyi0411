18b4287ee8a457f0d041af69911dbf5b3c14a94d
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-11 13:47:55 +0200

    10. Fibonacci completed

---
+++ week-02/day-4/10. Fibonacci/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int fibonacci (int n);
+
+int main() {
+
+    //Define a recursive fibonacci(n) method that returns the nth fibonacci number,
+    // with n=0 representing the start of the sequence.
+
+    std::cout << fibonacci(12) << std::endl;
+
+    return 0;
+}
+
+int fibonacci (int n) {
+    if (n >= 0) {
+        if (n > 2) {
+            return (fibonacci(n - 2) + fibonacci(n - 1));
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
