b90cb6f47ca0abd5b16a9e7b2e86971e78aeeb5a
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-12 09:02:33 +0200

    Alternate solution added

--- week-02/day-4/4. Greatest Common Divisor/main.cpp
+++ week-02/day-4/4. Greatest Common Divisor/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 
-int lnko_Recursively (int a, int b);
+int greatestCommonDivisor_Recursively(int a, int b);
+int greatestCommonDivisor_Recursively2(int a, int b);
 
 int main() {
 
     //Find the greatest common divisor of two numbers using recursion.
-
-    std::cout << lnko_Recursively (121, 33) << std::endl;
+    int a = 121;
+    int b = 33;
+    std::cout << greatestCommonDivisor_Recursively(121, 33) << std::endl;
+    std::cout << greatestCommonDivisor_Recursively2(121, 33) << std::endl;
+    std::cout << b % a;
 
     return 0;
 }
 
-int lnko_Recursively (int a, int b) {
+int greatestCommonDivisor_Recursively(int a, int b) {
     if (a == 0)
         return b;
-    return lnko_Recursively(b % a, a);
+    return greatestCommonDivisor_Recursively(b % a, a);
+}
+
+int greatestCommonDivisor_Recursively2(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return greatestCommonDivisor_Recursively2(b, a % b);
 }
