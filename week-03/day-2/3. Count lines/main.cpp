3813ee49f2b87dad1c3e3d35eaa5d2bda1212492
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-16 13:17:20 +0200

    3. Count lines completed

---
+++ week-03/day-2/3. Count lines/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <exception>
+
+int numberOfLines(std::string);
+
+int main()
+{
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file
+
+    std::cout << "Number of lines: " << numberOfLines("../Count lines.txt");
+
+    return 0;
+}
+
+int numberOfLines(std::string fileName)
+{
+    std::ifstream myFile;
+    myFile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+    int numberOfLines = 0;
+    try {
+        myFile.open(fileName);
+        while (!myFile.eof()) {
+            std::string text;
+            getline(myFile, text);
+            numberOfLines++;
+        }
+    }
+    catch (std::ofstream::failure &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    myFile.close();
+
+    return numberOfLines;
+}
