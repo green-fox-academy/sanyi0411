54f70e251e56d115da10c5e1407ae16d734fd8bc
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-04-17 14:30:21 +0200

    2. BlogPost completed

---
+++ week-03/day-3/2. BlogPost/BlogPost.cpp
@@ -0,0 +1,55 @@
+//
+// Created by sando on 2019. 04. 17..
+//
+
+#include "BlogPost.h"
+#include <iostream>
+
+BlogPost::BlogPost (std::string authorName, std::string title, std::string text, std::string publicationDate)
+{
+    _authorName = authorName;
+    _title = title;
+    _text = text;
+    _publicationDate = publicationDate;
+
+}
+
+void BlogPost::setAuthorName(std::string authorName)
+{
+    _authorName = authorName;
+}
+
+void BlogPost::setTitle(std::string title)
+{
+    _title = title;
+}
+
+void BlogPost::setText(std::string text)
+{
+    _text = text;
+}
+
+void BlogPost::setPublicationDate(std::string publicationDate)
+{
+    _publicationDate = publicationDate;
+}
+
+std::string BlogPost::getAuthorName()
+{
+    return _authorName;
+}
+
+std::string BlogPost::getTitle()
+{
+    return _title;
+}
+
+std::string BlogPost::getText()
+{
+    return _text;
+}
+
+std::string BlogPost::getPublicationDate()
+{
+    return _publicationDate;
+}
