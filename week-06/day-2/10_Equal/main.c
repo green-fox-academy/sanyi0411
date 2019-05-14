3eced0f41dcce2dd839bd0a928adb913225de4ef
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-05-14 14:05:52 +0200

    10. Equal completed

---
+++ week-06/day-2/10_Equal/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int areTheyEqual (int a, int b);
+
+int main()
+{
+    // Create a program which asks for two integers and stores them separatly
+    // Create a function which takes two numbers as parameters and
+    // returns 1 if they are equal and returns 0 otherwise
+
+    int a;
+    int b;
+
+    printf("Please enter the two numbers\nFirst number:");
+    scanf("%d", &a);
+    printf("Second number:");
+    scanf("%d", &b);
+
+    if(areTheyEqual(a, b)) printf("They are equal\n");
+    else printf("They are not equal\n");
+
+    return 0;
+}
+
+int areTheyEqual (int a, int b)
+{
+    if (a == b) return 1;
+    else return 0;
+}
