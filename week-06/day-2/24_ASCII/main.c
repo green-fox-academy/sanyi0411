1c46ec48df149acc345fb0e2923a04337400b084
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-05-14 16:23:51 +0200

    24. ASCII completed

---
+++ week-06/day-2/24_ASCII/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// print out the characters that corresponds to these ascii values
+
+int main()
+{
+    int array[] = {103, 114, 101, 101, 110, 32, 102, 111, 120};
+
+    for (int i = 0; i < sizeof(array) / sizeof(int); ++i) {
+        char c = array[i];
+        printf("%c", c);
+    }
+
+    return 0;
+}
