6fa146b40290d9bc372bad2157f9899bca2cc2b5
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-05-14 13:26:30 +0200

    2. Hello others completed

---
+++ week-06/day-2/2_Hello_others/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, Szucsi!\n");
+    printf("Hello, Anna!\n");
+    printf("Hello, Balazs!\n");
+
+    return 0;
+}
