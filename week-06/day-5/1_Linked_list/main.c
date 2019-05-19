0b913b01724815e9c508b49a584ed17e028a21e3
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-05-19 09:22:00 +0200

    Delete functions completed

--- week-06/day-5/1_Linked_list/main.c
+++ week-06/day-5/1_Linked_list/main.c
@@ -6,7 +6,7 @@ typedef struct linked_list_t {
     struct linked_list_t *next;
 } linked_list_t;
 
-linked_list_t *create_list();
+linked_list_t *create_linked_list();
 
 void insert_end(linked_list_t *head, int data);
 
@@ -18,9 +18,15 @@ int size_of_list(linked_list_t *head);
 
 void print_list(linked_list_t *head);
 
+int is_empty(linked_list_t *head);
+
+void delete_at_index(linked_list_t *head, int index);
+
+int delete_by_value(linked_list_t *head, int value);
+
 int main()
 {
-    linked_list_t *head = create_list();
+    linked_list_t *head = create_linked_list();
 
     insert_end(head, 62);
 
@@ -34,14 +40,19 @@ int main()
 
     insert_at_index(head, 72, 3);
 
+    printf("We deleted %d node(s)\n", delete_by_value(head, 55));
     print_list(head);
 
-    printf("The size of the list is:%d", size_of_list(head));
+    if(!is_empty(head)) {
+        printf("The size of the list is:%d", size_of_list(head));
+    } else {
+        printf("The list is empty");
+    }
 
     return 0;
 }
 
-linked_list_t *create_list()
+linked_list_t *create_linked_list()
 {
     linked_list_t *head = (linked_list_t *) malloc(sizeof(linked_list_t));
     head->data = 0;
@@ -109,4 +120,43 @@ int size_of_list(linked_list_t *head)
         size++;
     }
     return size;
+}
+
+int is_empty(linked_list_t *head)
+{
+    if (head->next == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void delete_at_index(linked_list_t *head, int index)
+{
+    linked_list_t *iterator = head;
+    for (int i = 0; i < index - 1; ++i) {
+        iterator = iterator->next;
+    }
+
+    linked_list_t *deleted_item = iterator->next;
+    iterator->next = iterator->next->next;
+
+    free(deleted_item);
+}
+
+int delete_by_value(linked_list_t *head, int value)
+{
+    int deleted_nodes = 0;
+    linked_list_t *iterator = head;
+    while (iterator->next != NULL) {
+        if (iterator->next->data == value) {
+            linked_list_t *deleted_item = iterator->next;
+            iterator->next = iterator->next->next;
+
+            free(deleted_item);
+            deleted_nodes++;
+        }
+        if(iterator->next != NULL)
+            iterator = iterator->next;
+    }
+    return deleted_nodes;
 }
