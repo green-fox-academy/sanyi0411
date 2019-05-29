62005b973df2eef3433ff9700122f5288b0edb59
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-05-29 15:44:40 +0200

    2. Receive some data completed

---
+++ week-08/day-3/2_Receive_some_data/inc/SystemClockConfig.h
@@ -0,0 +1,10 @@
+#ifndef SYSTEMCLOCKCONFIG_H_
+#define SYSTEMCLOCKCONFIG_H_
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+void Error_Handler(void);
+void SystemClock_Config(void);
+
+#endif /* SYSTEMCLOCKCONFIG_H_ */
