b5185ac4e97a94dd489c734c1d2a865d5dc70038
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-06-20 13:07:50 +0200

    bug fix

--- project_phase/0.week_project/test1.cpp
+++ project_phase/0.week_project/test1.cpp
@@ -4,14 +4,14 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-#define windowRows 500
-#define windowCols 500 
+const int windowColumns = 1000;
+const int windowRows = 500;
 
-cv::Mat image(windowRows, windowCols, CV_8UC3, CV_RGB(255, 255, 255));
+cv::Mat image(windowRows, windowColumns, CV_8UC3, CV_RGB(255, 255, 255));
 
 double findAngle(int x, int y)
 {
-	double b = (x - (windowCols / 2));
+	double b = (x - (windowColumns / 2));
 	if (b < 0)
 		b *= -1;
 	double a = (y - (windowRows / 2));
@@ -25,13 +25,13 @@ double findAngle(int x, int y)
 
 	double alpha = atan(tg_alpha) * 57.2957795; //Radian to degree conversion
 
-	if (x < (windowCols / 2) && y <= (windowRows / 2)) {
+	if (x < (windowColumns / 2) && y <= (windowRows / 2)) {
 		alpha = 90 - alpha;
 		alpha += 90;
 	}
-	if (x < (windowCols / 2) && y >(windowRows / 2))
+	if (x < (windowColumns / 2) && y >(windowRows / 2))
 		alpha += 180;
-	if (x >= (windowCols / 2) && y > (windowRows / 2)) {
+	if (x >= (windowColumns / 2) && y > (windowRows / 2)) {
 		alpha = 90 - alpha;
 		alpha += 270;
 	}
@@ -44,11 +44,11 @@ void myMouseCallback(int event, int x, int y, int flags, void* userdata)
 
 		image = CV_RGB(255, 255, 255);
 
-		cv::circle(image, { windowRows / 2, windowCols / 2 }, 5, 0, 2);
+		cv::circle(image, { windowColumns / 2, windowRows / 2 }, 5, 0, 2);
 
-		cv::line(image, { windowRows / 2, windowCols / 2 }, { windowRows, windowCols / 2 }, CV_RGB(0, 0, 0));
+		cv::line(image, { windowColumns / 2, windowRows / 2 }, { windowColumns, windowRows / 2 }, CV_RGB(0, 0, 0));
 
-		cv::line(image, { windowRows / 2, windowCols / 2 }, { x, y }, CV_RGB(0, 0, 0));
+		cv::line(image, { windowColumns / 2, windowRows / 2 }, { x, y }, CV_RGB(0, 0, 0));
 
 		//Double to string conversion:
 		std::ostringstream strs;
@@ -63,9 +63,9 @@ void myMouseCallback(int event, int x, int y, int flags, void* userdata)
 
 int main()
 {
-	cv::circle(image, { windowRows / 2, windowCols / 2 }, 5, 0, 2);
+	cv::circle(image, { windowColumns / 2, windowRows / 2 }, 5, 0, 2);
 
-	cv::line(image, { windowRows / 2, windowCols / 2 }, { windowRows, windowCols / 2 }, CV_RGB(0, 0, 0));
+	cv::line(image, { windowColumns / 2, windowRows / 2 }, { windowColumns, windowRows / 2 }, CV_RGB(0, 0, 0));
 
 	cv::imshow("Ablak", image);
 	cv::setMouseCallback("Ablak", myMouseCallback);
