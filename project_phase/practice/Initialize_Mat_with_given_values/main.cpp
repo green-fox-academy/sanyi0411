b4ee3df8c13488771b37a2b5429269adfb1779f6
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-07-01 16:02:23 +0200

    practice

--- project_phase/practice/Initialize_Mat_with_given_values/main.cpp
+++ project_phase/practice/Initialize_Mat_with_given_values/main.cpp
@@ -6,6 +6,12 @@ cv::Mat test = (cv::Mat_<char>(3, 3) <<
 	2, 4, 2,
 	2, 4, 0);
 
+//////////////////////////////////////
+
+float data[9] = {1,2,3,4,5,6,7,8,9};
+cv::Mat test2 = cv::Mat(3, 3, CV_32F, data);
+
+
 int main(int argc, char** argv)
 {
 	//Access single pixel:
@@ -13,6 +19,7 @@ int main(int argc, char** argv)
 
 	//Print out the hole matrix
 	std::cout << test << std::endl;
+	std::cout << test2 << std::endl;
 
 	return 0;
 }
