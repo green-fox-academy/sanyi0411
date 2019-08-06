63e9493219904faf208355393b348877e97b97e4
sanyi0411 <47214930+sanyi0411@users.noreply.github.com>
2019-08-06 13:15:41 +0200

    practice

--- OpenCV/practice/Serial_communication/main.cpp
+++ OpenCV/practice/Serial_communication/main.cpp
@@ -11,6 +11,15 @@ int main(int argc, char** argv)
     std::cout << "Is connected: " << std::boolalpha << car->isConnected() << std::endl;
     std::string command = "motor forward 50 1\n";
     std::cout << "Data transfer: " << std::boolalpha << car->writeSerialPort(command.c_str(), 20) << std::endl;
+    
+    /*We need to wait for the answer. If we don't wait, we will try to read the serial port faster than the other can answer*/
+    int input = cv::waitKey(200);
+
+    char receivedString[255];
+    int hasRead = car->readSerialPort(receivedString, 255);
+    if (hasRead) {
+        std::cout << receivedString << std::endl;
+    }
 
     return 0;
 }
